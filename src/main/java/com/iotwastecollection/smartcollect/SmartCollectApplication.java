c9f691c26d5c8a515505805cd33b9b93da960235
Ahmimou <aymanahmimou9@gmail.com>
2025-11-15 16:47:45 +0100

    init

---
+++ src/main/java/com/iotwastecollection/smartcollect/SmartCollectApplication.java
@@ -0,0 +1,13 @@
+package com.iotwastecollection.smartcollect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCollectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCollectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iotwastecollection/smartcollect/SmartCollectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iotwastecollection.smartcollect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCollectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
